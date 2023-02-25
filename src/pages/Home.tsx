dc0fd31f71fd626da3c27f402ca4af958e184fab
officialyenum <oponechukwuyenum@gmail.com>
2023-02-25 19:07:36 +0100

    FIX: Clean Up Unused Elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import Dashboard from '../components/Dashoard';
-import { Tabs, TabList, Tab, TabPanels,TabPanel, Heading, Box, Container } from '@chakra-ui/react';
+import { Tabs, TabList, Tab, TabPanels,TabPanel, Container } from '@chakra-ui/react';
 import Rewrite from '../components/Rewrite';
 import Nav from '../components/Nav';
 import Summarize from '../components/Summarize';
@@ -10,66 +10,32 @@ interface HomeProps {
 }
 
 const Home: FC<HomeProps> = () => {
-    const [showDashboard, setShowDashboard] = useState(true)
-    const [showSummarize, setShowSummarize] = useState(false)
-    const [showRewrite, setShowRewrite] = useState(false)
-    const [showParaphrase, setShowParaphrase] = useState(false)
+    return (
+        <>
+            <Container maxW={['sm','md', 'xl']}>
+                <Nav/>
+                <Tabs>
+                    <TabList>
+                        <Tab>Dashboard</Tab>
+                        <Tab>Rewrite</Tab>
+                        <Tab>Summarize</Tab>
+                    </TabList>
 
-    const onHandleClick = (componentName:string) => {
-        cleanActive();
-        switch (componentName) {
-            case "Summarize":
-                console.log(componentName);
-                setShowSummarize(true);
-                break;
-            case "Rewrite":
-                setShowRewrite(true);
-                console.log(componentName);
-                break;
-            case "Paraphrase":
-                setShowParaphrase(true);
-                console.log(componentName);
-                break;
-            default:
-                setShowDashboard(true);
-                console.log(componentName);
-                break;
-        }
-    }
-
-    const cleanActive = () => {
-        setShowDashboard(false);
-        setShowSummarize(false);
-        setShowRewrite(false);
-        setShowParaphrase(false);
-    }
-
-  return (
-    <>
-        <Container maxW={['sm','md', 'xl']}>
-            <Nav/>
-            <Tabs>
-                <TabList>
-                    <Tab>Dashboard</Tab>
-                    <Tab>Rewrite</Tab>
-                    <Tab>Summarize</Tab>
-                </TabList>
-
-                <TabPanels>
-                    <TabPanel>
-                        <Dashboard title={'Home Page'} />
-                    </TabPanel>
-                    <TabPanel>
-                        <Rewrite/>
-                    </TabPanel>
-                    <TabPanel>
-                        <Summarize/>
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
-        </Container>
-    </>
-  );
+                    <TabPanels>
+                        <TabPanel>
+                            <Dashboard title={'Home Page'} />
+                        </TabPanel>
+                        <TabPanel>
+                            <Rewrite/>
+                        </TabPanel>
+                        <TabPanel>
+                            <Summarize/>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </Container>
+        </>
+    );
 };
 
 export default Home;
