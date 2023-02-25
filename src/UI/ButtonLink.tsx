03dfa7faeaf5db8a9d20731a5679e1cd7a6dff8c
officialyenum <oponechukwuyenum@gmail.com>
2023-02-25 22:22:13 +0100

    Added Links to Documents

---
+++ src/UI/ButtonLink.tsx
@@ -0,0 +1,17 @@
+import { Link, textDecoration } from '@chakra-ui/react';
+import React, { FC } from 'react';
+
+
+const ButtonLink:FC<any> = ({ChakraComponent, href, children, ...props}) => {
+  return (
+    <Link href={href} target={'_blank'} _hover={{
+        textDecoration: 'none'
+    }}>
+        <ChakraComponent {...props}>
+            {children}
+        </ChakraComponent>
+    </Link>
+  )
+}
+
+export default ButtonLink
--- src/components/Dashoard.tsx
+++ src/components/Dashoard.tsx
@@ -2,6 +2,7 @@
 import { ArrowRightIcon } from '@chakra-ui/icons';
 import { Box, Heading, Text, Button, Stack } from '@chakra-ui/react';
 import React, { FC } from 'react';
+import ButtonLink from '../UI/ButtonLink';
 // import paraphraseLogo from '../assets/paraphrase.png';
 // import summarizeLogo from '../assets/summarize.png';
 // import rewriteLogo from '../assets/rewrite.png';
@@ -15,17 +16,22 @@ const Dashboard: FC<DashboardProps> = ({title}) => {
     return (
         <>
             <Box maxW='32rem'>
-                <Heading fontSize={['3xl','4xl']} mb={4}>Yenum AI Studio</Heading>
+                <Heading fontSize={['3xl','4xl']} mb={4}>Yenum AI Playground</Heading>
                 <Text fontSize={['md','lg']}>
-                    Yenum AI utilizes AI 21 Studio NPM Package, and help you Rewrite, Summarize and Paraphrase your articles
+                    Yenum AI utilizes AI 21 Studio NPM Package manager, and helps you process natural language models like Rewriting or Paraphrase a text in multiple ways, Summarize an article or documents etc.
+                    This Playground is for you to see the potential of the package i built. I hope you enjoy it...
                 </Text>
                 <Stack direction='row'  mt={10}>
-                    <Button bgColor={'blue.400'} size={['md','lg']}>
-                        <span>Get AI 21 Key - </span> <ArrowRightIcon fontSize={'md'}/>
-                    </Button>
-                    <Button bgColor={'red.400'} size={['md','lg']}>
-                        <span>Read Doc - </span> <ArrowRightIcon fontSize={'md'}/>
-                    </Button>
+                    <ButtonLink 
+                        href={'https://studio.ai21.com/overview'} 
+                        ChakraComponent={Button}  
+                        bgColor={'blue.400'} 
+                        size={['md','lg']}>
+                        <Text fontSize={'xs'}>Learn more about AI 21 - </Text> <ArrowRightIcon/>
+                    </ButtonLink>
+                    <ButtonLink href={'https://www.npmjs.com/package/@officialyenum/ai21'} ChakraComponent={Button}  bgColor={'red.400'} size={['md','lg']}>
+                        <Text fontSize={'xs'}>Read Npm Package Doc - </Text> <ArrowRightIcon/>
+                    </ButtonLink>
                 </Stack>
             </Box>
         </>
